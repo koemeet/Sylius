a9a9db137fa94e5e25fc3d1a8ac3f52df22cfcf1
Steffen Brem <steffenbrem@gmail.com>
2017-02-03 13:57:33 +0100

    Return whole resource in update response.

--- src/Sylius/Bundle/ResourceBundle/Controller/ResourceController.php
+++ src/Sylius/Bundle/ResourceBundle/Controller/ResourceController.php
@@ -318,7 +318,7 @@ class ResourceController extends Controller
             $this->eventDispatcher->dispatchPostEvent(ResourceActions::UPDATE, $configuration, $resource);
 
             if (!$configuration->isHtmlRequest()) {
-                return $this->viewHandler->handle($configuration, View::create(null, Response::HTTP_NO_CONTENT));
+                return $this->viewHandler->handle($configuration, View::create($resource, Response::HTTP_OK));
             }
 
             $this->flashHelper->addSuccessFlash($configuration, ResourceActions::UPDATE, $resource);
